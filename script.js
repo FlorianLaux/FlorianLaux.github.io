83a960588e49eec3276cbb27a7036fbf0c94344e
Florian Laux <Florian.Laux@ou.edu>
2019-05-01 15:40:58 -0500

    Added collabsible

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ function openCity(evt, cityName) {
   }
 
   window.onload = openCity(null, "Home");
+
+  var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
